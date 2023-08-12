6ea6d49c2e12b2115522fddecbe18c0234d3d3bd
omarFlores <omar710.jimflo@gmail.com>
2023-08-12 16:00:05 -0600

    Cambios 12/08/2023

--- SRC/index.js
+++ SRC/index.js
@@ -3,7 +3,7 @@ import app  from "./app.js";
 const main = () =>{
     const PORT = app.get("PORT");
 
-    app.listen(PORT, () =>{
+    app.listen(PORT, "0.0.0.0",() =>{
         console.log(`Servidor en puerto ${PORT}`);
     });
 }
